f3b98c8308dba120d5dc11163706d6ff9e76cbd0
caleshiad <caleshiad@gmail.com>
2026-06-04 16:32:12 -0400

    Chapter 5 Python Strings Notes

---
+++ Python Strings.py
@@ -0,0 +1,7 @@
+# String Comparison and String Case 
+
+#string comparisons let Python determine whether two pieces of text are identical or distinct 
+"cat" == "cat" #Output will be True, because both strings are identical
+"cat" == "Cat" #Output will be False, because the first letter is different (lowercase vs uppercase)
+"apple" < "banana" #Output will be True, because "a" comes before "b" in the alphabet
+
